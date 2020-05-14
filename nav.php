1c86e770575a764b8518bc2ab217eef46f65e3ed
tuyenhoang0320i1 <hoangtuyen9x@gmail.com>
2020-05-15 00:21:19 +0700

    add file lab

---
+++ nav.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+    nav {
+        float: left;
+        max-width: 160px;
+        margin: 0;
+        padding: 1em;
+    }
+    nav ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    nav ul a {
+        text-decoration: none;
+    }
+</style>
+<body>
+<ul>
+    <li><a href="#">London</a> </li>
+    <li><a href="#">Paris</a> </li>
+    <li><a href="#">Tokyo</a> </li>
+</ul>
+</body>
+</html>
